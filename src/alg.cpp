bc4d2002537130201cf59e05bd6933725bc5e485
jollynavechno <77914359+jollynavechno@users.noreply.github.com>
2021-05-13 13:19:36 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,6 @@
 // Copyright 2021 NNTU-CS
 #include <iostream>
+#include <string>
 #include "tstack.h"
 
 int Priority(char c) {
@@ -20,7 +21,9 @@ for (int i = 0; i < inf.length(); ++i) {
 if ((inf[i] >= '0') && (inf[i] <= '9')) {
 pst += inf[i];
 pst += ' ';
-} else if ((inf[i] == '(') || (cstack.isEmpty()) || (Priority(inf[i]) > Priority(cstack.get()))) {
+} else if ((inf[i] == '(') 
+|| (cstack.isEmpty()) 
+|| (Priority(inf[i]) > Priority(cstack.get()))) {
 cstack.push(inf[i]);
 } else if (inf[i] == ')') {
 char ch = cstack.get();
@@ -54,19 +57,19 @@ for (int i = 0; i < pst.length(); i++) {
 if (pst[i] >= '0' && pst[i] <= '9')
 istack.push(pst[i] - '0');
 else if (pst[i] != ' ') {
-int var1 = istack.get();
-istack.pop();
-int var2 = istack.get();
-istack.pop();
-if (pst[i] == '*')
-istack.push(var2 * var1);
-else if (pst[i] == '/')
-istack.push(var2 / var1);
-else if (pst[i] == '+')
-istack.push(var2 + var1);
-else
-istack.push(var2 - var1);
-}
-}
+ int var1 = istack.get();
+ istack.pop();
+ int var2 = istack.get();
+ istack.pop();
+ if (pst[i] == '*')
+  istack.push(var2 * var1);
+ else if (pst[i] == '/')
+  istack.push(var2 / var1);
+ else if (pst[i] == '+')
+  istack.push(var2 + var1);
+ else
+  istack.push(var2 - var1);
+ }
+ }
 return istack.get();
 }
